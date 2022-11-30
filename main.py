da40a0bfc49206c30f1d2934fbc86d1f79687e2b
Jocec31 <revy.cecile@gmail.com>
2022-11-30 08:10:17 +0100

    streamlit vers github

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas
+
+st.title('App avec Streamlit')
+st.subheader('Présentation d\'un dashboard')
+st.write('''Ceci est ma première application avec streamlit.
+         Profitez du moment
+         ''')
