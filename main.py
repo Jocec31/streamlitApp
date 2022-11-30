0c295ca6df58b68b75e9d03bfe1c8b4948f08ed1
Jocec31 <revy.cecile@gmail.com>
2022-11-30 15:30:36 +0100

    map

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 data = {
     'Series-1': [1,2,3,4,5,6],
@@ -23,3 +24,9 @@ st.area_chart(df) # graph de zones
 my_slider = st.slider('Celsius')
 st.write(my_slider, 'in Farenheit is ', my_slider*9/5+32)
 
+# map
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
